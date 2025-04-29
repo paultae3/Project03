c6573d41671bf36376c9197b435b1aa1a0ae7787
paultae3 <paultae208@gmail.com>
2025-04-29 03:06:51 -0500

    Update Map
    Tried to update the controller but it doesn't work.

---
+++ Assets/_Game/Scripts/UrbanEagleController.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class UrbanEagleController : MonoBehaviour
+{
+    private InputHandler _input;
+
+    public GameObject _eagle;
+
+    public float _gravity = 30;
+
+    public float _jump = 10;
+
+    private float _verticalSpeed;
+
+
+    void Start()
+    {
+        
+    }
+
+   
+    void Update()
+    {
+
+        _verticalSpeed += -_gravity * Time.deltaTime;
+
+        if (Input.GetKeyDown(KeyCode.Space ))
+        {
+            _verticalSpeed = 0;
+            _verticalSpeed += _jump;
+        }
+
+        _eagle.transform.position += Vector3.up * _verticalSpeed * Time.deltaTime;
+
+    }
+}
